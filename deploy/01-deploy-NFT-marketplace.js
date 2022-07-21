e2651720e4b1f321ac2f789c83ae1c4ea812cd09
SachinCoder1 <sachinhlo232@gmail.com>
2022-07-21 10:56:00 +0530

    deploy script for NFT marketplace

---
+++ deploy/01-deploy-NFT-marketplace.js
@@ -0,0 +1,35 @@
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+
+const logging = (text) => {
+  return text + " ---------------------------";
+};
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+
+  args = [];
+
+  log(logging("Started Deploying NFTMarketplace"));
+
+  const NFTMarketplace = await deploy("NFTMarketplace", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  log(logging("Successfully deployed NFTMarketplace"));
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log(logging("verifying"));
+    verify(NFTMarketplace.address);
+  }
+};
